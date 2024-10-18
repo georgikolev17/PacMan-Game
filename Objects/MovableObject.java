c73b39d97c06b468ea28c964c0333741d590b742
Georgi Kolev <g.t.kolev1@gmail.com>
2024-10-18 14:18:44 +0200

    Added timer to Game and removed it from MovableObject

--- Objects/MovableObject.java
+++ Objects/MovableObject.java
@@ -6,7 +6,7 @@ import java.awt.event.ActionListener;
 import javax.swing.JPanel;
 import javax.swing.Timer;
 
-public abstract class MovableObject extends JPanel implements ActionListener {
+public abstract class MovableObject extends JPanel {
     protected final int STEP = 5;
     protected int dx = STEP;
     protected int dy = 0;
@@ -20,22 +20,29 @@ public abstract class MovableObject extends JPanel implements ActionListener {
         setOpaque(false);  // Make the Pacman panel transparent
         this.setBounds(0, 0,910, 630);  // Set the position and size explicitly
 
-        // Calls the actionPerformed() function every 30ms
-        timer = new Timer(30, this);  // Slower timer for smoother movement
-        timer.start();
+        
     }
 
-    @Override
-    public void actionPerformed(ActionEvent e) {
+    /**
+     * Summary: The step every movable object makes on every period of time (for now 30ms).
+     */
+    public void step() {
         x += this.dx;
         y += this.dy;
-
+        // System.out.println(y / 30 + " " + x / 30);
         // Ensure the object stays within panel bounds
-        if (x < 0) x = 0;
-        if (y < 0) y = 0;
-        if (x + objSize > getWidth()) x = getWidth() - objSize;
-        if (y + objSize > getHeight()) y = getHeight() - objSize;
-
+        if (x < 0) {
+            x = 0;
+        }
+        if (y < 0) {
+            y = 0;
+        }
+        if (x + objSize > getWidth()) {
+            x = getWidth() - objSize;
+        }
+        if (y + objSize > getHeight()) {
+            y = getHeight() - objSize;
+        }
         // Repaint the object
         repaint();
     }
